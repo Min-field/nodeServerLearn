498086c33f5bf6637cd786bc2a5e1313433d9954
吴斐 <wufei@wufeideMacBook-Pro.local>
2017-08-15 17:06:05 +0800

    node 原生服务器添加路由

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(request, response){
     var path = request.url.replace(/\/?(\?.*)?$/, '');
     console.log(path);
     switch (path){
         case '': 
-            getRes(response, 'home');
+            getRes(response, '/public/home.html', 'text/html');
             break;
         case '/about':
-            getRes(response, 'about');
+            getRes(response, '/public/about.html', 'text/html');
             break;
-        case '/404':
-            getRes(response, '404');
-            break;
+        case './public/logo.jpg':
+            getRes(response, '/public/logo,jpg', 'image/jpeg')
         default: 
-            getRes(response, '404');
+            getRes(response, '404', 'text/html', 404);
     }
 }).listen(3000);
 
-function getRes(res, str){
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end(str);
+function getRes(res, path, contentType, responseCode){
+    fs.readFile(path, (err, data) => {
+        if(err){
+            res.writeHead(500, {'Content-type': 'text/plain'});
+            res.end('500 - Internal Error');
+        } else {
+            if(!responseCode){
+                res.writeHead(200, {'Content-type': contentType});
+            } else {
+                res.writeHead(responseCode, {'Content-type': contentType});
+            }
+            res.end(data);
+        }
+    })
 }
 console.log('Server started in localhost:3000; press "Ctrl-C" to terminating');
