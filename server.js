2c0406641176ae235eb8ce4083ea305b44088bde
吴斐 <wufei@wufeideMacBook-Pro.local>
2017-08-15 16:49:21 +0800

    node 原生服务器添加路由

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 var http = require('http');
 
 http.createServer(function(request, response){
-    response.writeHead(200, { 'Content-type': 'text/plain'});
-    response.end('Hello world');
+    var path = request.url.replace(/\/?(\?.*)?$/, '');
+    console.log(path);
+    switch (path){
+        case '': 
+            getRes(response, 'home');
+            break;
+        case '/about':
+            getRes(response, 'about');
+            break;
+        case '/404':
+            getRes(response, '404');
+            break;
+        default: 
+            getRes(response, '404');
+    }
 }).listen(3000);
+
+function getRes(res, str){
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end(str);
+}
 console.log('Server started in localhost:3000; press "Ctrl-C" to terminating');
